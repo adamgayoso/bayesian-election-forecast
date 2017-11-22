43514bd3db79f74b7f08369572fd7c6356070cec
adamgayoso <adamgayoso@users.noreply.github.com>
2017-11-22 17:37:14 -0500

    vectorized plots

--- src/plots.py
+++ src/plots.py
@@ -17,59 +17,39 @@ ELECTION_DATE = dt.date(2016, 11, 8)
 
 def generate_time_plot(state_scores, state_s_polls, burn_in, state_name, prior, save=False):
 
-    medians = []
-
-    means = []
-    lowers = []
-    uppers = []
-    sigmas = []
-    x_coord_date = []
-    x_coord = []
-    for day in range(0, 191):
-        x_coord.append(day)
-        # x_coord_date.append()
+    n_days = state_scores.shape[0]
+    x_coord = np.arange(n_days)
+    # x_coord_date.append()
     # median of posterior - thick blue
     # light-blue region = 90% credible interval
     # thin-blue lines = 100 draws from posterior
 
-    samples_chosen = 100
-    sample_size = 10000
-    my_randoms = random.sample(range(burn_in, sample_size), samples_chosen)
+    # samples_chosen = 100
+    # sample_size = 10000
+    # my_randoms = random.sample(range(burn_in, sample_size), samples_chosen)
     burn_state_scores = state_scores[:, burn_in:]
 
     # plot thin blue lines (100 posterior draws)
     # for r in my_randoms:
     #     y_coord=[]
-    #     for day in range(0,191):
+    #     for day in range(0,n_days):
     #         y_coord.append(state_scores[day][r])
     #     plt.plot(x_coord,y_coord,linewidth=0.05)
     # plot 90% confidence interval
-    x_conf = []
-    y_conf = []
 
     # plot dots (polls)
-    index_x = ([])
-    index_y = ([])
-    for po_pc in (state_s_polls.p_clinton):
-        index_y.append(po_pc)
-    for po_date in (state_s_polls.poll_date):
-        time_from_start = abs(po_date.date() - START_DATE)
-        index_x.append(time_from_start.days)
-    sizes_s = [10 for n in range(len(index_x))]
-    plt.scatter(index_x, index_y, s=sizes_s, c='black')
+    poll_date = state_s_polls.date_index.as_matrix()
+    poll_value = state_s_polls.p_clinton.as_matrix()
+    plt.scatter(poll_date, poll_value, s=10, c='black')
 
     # plot thick blue line (median) and 90% confidence interval
-    for day in range(0, 191):
-        day_scores = burn_state_scores[day]
-        day_median = np.median(day_scores)
-        day_mean, day_sigma = np.mean(day_scores), np.std(day_scores)
-        conf_int = stats.norm.interval(0.9, loc=day_mean, scale=day_sigma)
-        lowers.append(conf_int[0])
-        uppers.append(conf_int[1])
-        medians.append(day_median)
-        sigmas.append(day_sigma)
-        means.append(day_mean)
-    plt.ylabel('Pr(Clinton wins)' + state_name)
+    medians = np.median(burn_state_scores, axis=1)
+    uppers = np.percentile(burn_state_scores, 95, axis=1)
+    lowers = np.percentile(burn_state_scores, 5, axis=1)
+
+    plt.title('Clinton Vote Share over Time for ' + state_name.title())
+    plt.ylabel('Share of Two-Party Vote')
+    plt.xlabel('Day')
     plt.plot(x_coord, medians, color='blue')
     plt.plot(x_coord, lowers, color='#33CEFF')
     plt.plot(x_coord, uppers, color='#33CEFF')
@@ -77,7 +57,6 @@ def generate_time_plot(state_scores, state_s_polls, burn_in, state_name, prior, 
                      interpolate=True, color='#33CEFF', alpha=.4)
     plt.axhline(y=0.5, color='black', linestyle='-')
     plt.axhline(y=prior, color='black', linestyle='--')
-
 
     if save is True:
         plt.savefig('../plots/time_plots/' +
@@ -88,12 +67,13 @@ def generate_time_plot(state_scores, state_s_polls, burn_in, state_name, prior, 
 def generate_undecided_plot(undecided_table, state_index, state_name, mean_w, mean_b, E_day, save=False):
 
     s = state_index
+    state_name = state_name.title()
     date_range = np.arange(E_day)
     state = undecided_table[np.where(undecided_table[:, 2] == s)[0]]
     dates = state[:, 1]
     und = state[:, 0]
     plt.scatter(dates, und)
-    plt.title('Undecided Voters in ' + state_name)
+    plt.title('Undecided Voters in ' + state_name.title())
     plt.xlabel('Day index')
     plt.ylabel('Percantage Undecided')
     plt.plot(date_range, mean_w[s] * date_range + mean_b[s], c='orange')
